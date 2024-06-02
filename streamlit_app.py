4e63446688415ff507e644be2ce4741f13a6b615
Sahaj <sahajguptarocks@gmail.com>
2024-06-02 21:31:45 +0530

    ok

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
+# hume aapki zarurat hai
+# importing dependencies
 import streamlit as st
+import numpy as np
+import PIL as img
+import requests
+from io import BytesIO      # ye kya hai bc
 
-st.write("Hello World")
+# Title
+st.title("Restaurant Analysis System")
