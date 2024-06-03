9c419236cb403103a86ce665563b7c84d3445366
Sahaj <sahajguptarocks@gmail.com>
2024-06-03 15:44:23 +0530

    menu added

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 # hume aapki zarurat hai
 # importing dependencies
 import streamlit as st
+import time
 import numpy as np
 import pandas as pd
 import PIL as img
 import requests
 from io import BytesIO      # ye kya hai bc
+
 
 # Title
 st.title("Restaurant Analysis System")
@@ -19,3 +21,22 @@ map_data = pd.DataFrame(
     columns=['lat', 'lon'])
 
 st.map(map_data)
+
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
