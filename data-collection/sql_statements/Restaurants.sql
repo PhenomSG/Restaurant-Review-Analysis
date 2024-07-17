3f4b75949551aab50a4a771671d7f481f41266a5
Sahaj <sahajguptarocks@gmail.com>
2024-07-17 15:15:55 +0530

    redundancy removed

--- data-collection/sql_statements/Restaurants.sql
+++ data-collection/sql_statements/Restaurants.sql
@@ -40,15 +40,17 @@ INSERT INTO Restaurants (name, address, plus_code) VALUES
 ('Oota', '618, 12th Main Road, HAL 2nd Stage, Indiranagar, Bangalore', 'XH4W+4C Bangalore, Karnataka'),
 ('Vidyarthi Bhavan', '32, Gandhi Bazaar Main Road, Basavanagudi, Bangalore', 'WHX8+99 Bangalore, Karnataka'),
 ('Alto Vino', 'Whitefield Main Road, Bangalore', 'XH4R+7R Bangalore, Karnataka'),
-('Ssaffron', 'Sheraton Grand Bangalore Hotel, Brigade Gateway, Bangalore', 'WHX3+8M Bangalore, Karnataka'),
 ('Paranda', 'Taj Yeshwantpur, 2275, Tumkur Road, Bangalore', 'XH4R+7M Bangalore, Karnataka'),
 ('Peshawri', 'ITC Windsor, Golf Course Road, Bangalore', 'XH2J+5V Bangalore, Karnataka'),
 ('Masala Klub', 'Taj West End, Race Course Road, Bangalore', 'XH2J+4G Bangalore, Karnataka'),
 ('Lapis', 'The Oberoi, MG Road, Bangalore', 'XH2J+29 Bangalore, Karnataka'),
+('MTR', '14, Lalbagh Road, Near Basavanagudi, Bangalore', 'WHX3+3X Bangalore, Karnataka'),
+('New Krishna Bhavan', '33, Sampige Road, Malleshwaram, Bangalore', 'WHX6+G3 Bangalore, Karnataka'),
+('Sankalp', '22, 2nd Floor, Commercial Street, Bangalore', 'XH2J+7J Bangalore, Karnataka'),
+('Sattvam', '315, 100 Feet Road, Indiranagar, Bangalore', 'XH4V+5R Bangalore, Karnataka'),
+('Udupi Krishna Bhavan', '112, 2nd Main, 6th Cross, Gandhi Bazaar, Bangalore', 'WHX8+6M Bangalore, Karnataka'),
 ('Cubbon Pavilion', 'ITC Gardenia, Residency Road, Bangalore', 'XH2J+3G Bangalore, Karnataka'),
-('Ssaffron', 'Sheraton Grand Bangalore, Brigade Gateway, Bangalore', 'WHX3+8J Bangalore, Karnataka'),
-('Blue Ginger', 'The Taj West End, Race Course Road, Bangalore', 'XH2J+4R Bangalore, Karnataka');
-('Hakuna Matata','1231/35/2, Brigade Millenium Rd, opposite Brigade Palmsprings, Paradise Colony, JP Nagar 7th Phase, J. P. Nagar, Bengaluru, Karnataka 560078','VHVP+HJ Bengaluru, Karnataka')
-('Thalassery restaurant','1763, 14th Main Rd, 1st Stage, Kumaraswamy Layout, Bengaluru, Karnataka 560078','WH47+WH Bengaluru, Karnataka')
-('Udupi Swada','No.587, Ground and1st Floor, 14th Main Road, 20th Cross Rd, 1st Stage, Kumaraswamy Layout, Bengaluru, Karnataka 560078','WH57+2J Bengaluru, Karnataka')
-('Dwaraka Grand','Dayanand Sagar College Rd, Kumaraswamy Layout, Bengaluru, Karnataka 560078','WH58+V6 Bengaluru, Karnataka')
+('Hakuna Matata','1231/35/2, Brigade Millenium Rd, opposite Brigade Palmsprings, Paradise Colony, JP Nagar 7th Phase, J. P. Nagar, Bengaluru, Karnataka 560078','VHVP+HJ Bengaluru, Karnataka'),
+('Thalassery restaurant','1763, 14th Main Rd, 1st Stage, Kumaraswamy Layout, Bengaluru, Karnataka 560078','WH47+WH Bengaluru, Karnataka'),
+('Udupi Swada','No.587, Ground and1st Floor, 14th Main Road, 20th Cross Rd, 1st Stage, Kumaraswamy Layout, Bengaluru, Karnataka 560078','WH57+2J Bengaluru, Karnataka'),
+('Dwaraka Grand','Dayanand Sagar College Rd, Kumaraswamy Layout, Bengaluru, Karnataka 560078','WH58+V6 Bengaluru, Karnataka');
